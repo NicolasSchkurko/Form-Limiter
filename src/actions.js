edbc2d41d2affeb15ce4bc14b3f6d2b2c7bfb216
chuco-lapwin <schkurkonicolas@gmail.com>
2025-09-28 19:48:50 -0300

    Fixing errors

--- src/actions.js
+++ src/actions.js
@@ -8,17 +8,23 @@ var scriptProperties = PropertiesService.getScriptProperties();
 function setMaxResponses(limit) {
   try {
     var limitNumber = parseInt(limit, 10);
+
     if (!isNaN(limitNumber) && limitNumber > 0) {
       scriptProperties.setProperty('maxResponses', limitNumber.toString());
-      setTrigger(); // Ensure the onFormSubmit trigger exists.
+      setTrigger();
       return 'Limit set to ' + limitNumber + ' responses.';
     }
+
     throw new Error('Please enter a valid number greater than zero.');
+
   } catch (e) {
+
     if (e.message.includes('PERMISSION_DENIED')) {
       throw new Error('Permission Error: It seems you have multiple Google accounts logged in. Please log out of other accounts and try again.');
     }
+
     throw new Error(e.message);
+
   }
 }
 
@@ -29,12 +35,15 @@ function setMaxResponses(limit) {
  */
 function setConfirmationMessage(message) {
   try {
-    if (!message || message.trim() === '') {
+
+    if (messageIsEmtpy(message)) {
       throw new Error('Confirmation message cannot be empty.');
     }
+
     var form = FormApp.getActiveForm();
     form.setConfirmationMessage(message);
     return 'Confirmation message updated successfully.';
+
   } catch (e) {
     if (e.message.includes('PERMISSION_DENIED')) {
       throw new Error('Permission Error: It seems you have multiple Google accounts logged in. Please log out of other accounts and try again.');
@@ -50,11 +59,21 @@ function setConfirmationMessage(message) {
  */
 function setClosedMessage(message) {
   try {
-    if (!message || message.trim() === '') {
+    if (messageIsEmtpy(message)) {
       throw new Error('Closed form message cannot be empty.');
     }
     var form = FormApp.getActiveForm();
+    var wasClosed = !form.isAcceptingResponses();
+
+    if (wasClosed) {
+      form.setAcceptingResponses(true);
+    }
+
     form.setCustomClosedFormMessage(message);
+
+    if (wasClosed) {
+      form.setAcceptingResponses(false);
+    }
     return 'Closed form message updated successfully.';
   } catch (e) {
     if (e.message.includes('PERMISSION_DENIED')) {
@@ -62,5 +81,14 @@ function setClosedMessage(message) {
     }
     throw new Error(e.message);
   }
+}
+
+/**
+ * Verifies if a message it's empty.
+ * @param {string} message The evaluated message.
+ * @returns {boolean} The validation.
+ */
+function messageIsEmtpy(message){
+ return !message || message.trim() === ''
 }
 
